df5d229a87d8df65d6ff979d5e75d9226b92a01b
llong <1091852800@qq.com>
2025-11-28 06:45:24 +0000

    build: 打包策略更新

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,13 +3,14 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
-  dts: {
-    only: true, // 仅生成 .d.ts 文件（保留 tsc 生成 .js 文件）
-    resolve: true, // 解析外部类型
-  },
+  dts: true,
   outDir: 'dist',
-  clean: false, // 不清理 dist，因为 tsc 会在此处生成 .js 文件
+  clean: true,
   sourcemap: false,
   splitting: false,
-  treeshake: false,
+  treeshake: true,
+  minify: true,
+  esbuildOptions(options) {
+    options.drop = ['console']
+  },
 })
